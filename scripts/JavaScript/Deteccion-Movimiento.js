fb086e096fda39f9e869e9b6232b2c82a31aa1be
Juanvelandia-p <juan.velandia-p@mail.escuelaing.edu.co>
2025-04-15 13:49:29 -0500

    Adaptacion responsive para tabletas e implementacion de banco de carga de fondo

--- scripts/JavaScript/Deteccion-Movimiento.js
+++ scripts/JavaScript/Deteccion-Movimiento.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let lastScrollTop = 0;
+  let lastScrollTop = 0; // Última posición de desplazamiento
   const topBar = document.querySelector('.top-bar');
 
-  window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
     if (scrollTop > lastScrollTop) {
-      // Desplazando hacia abajo
+      // Desplazando hacia abajo: Oculta la barra
       topBar.classList.add('hidden');
     } else {
-      // Desplazando hacia arriba
+      // Desplazando hacia arriba: Muestra la barra
       topBar.classList.remove('hidden');
     }
-    lastScrollTop = scrollTop;
+
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Evita valores negativos
   });
 });
